88a6fb1ea63ddd9f26104bc2d4c8c103bcf635d1
iSkylake <skylakebeta@gmail.com>
2020-11-02 22:38:28 -0800

    Add user selection menu; add users to db; center menu item component

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react';
 import cls from './Navbar.module.css';
 import { StateContext } from '../../context/stateContext';
+import useDropdown from '../../hooks/useDropdown';
+import { ReactComponent as ExpandIcon } from '../../images/expand_more.svg';
 
 import ProfilePicture from '../common/ProfilePicture';
+import Menu from '../common/Menu';
+import MenuItem from '../common/MenuItem';
 
 function Navbar() {
-	const { currentUser } = useContext(StateContext).state;
+	const { currentUser, users } = useContext(StateContext).state;
+	const { open, dropRef, onOpen } = useDropdown(false);
 
 	return (
 		<nav className={cls.navbar}>
@@ -15,6 +20,21 @@ function Navbar() {
 			<div className={cls.itemCtn}>
 				<ProfilePicture color={currentUser.profilePic}/>
 				<h4>{currentUser.username}</h4>
+				<ExpandIcon className={cls.expandIcon} onClick={onOpen} ref={dropRef}/>
+				{
+					open && (
+						<Menu>
+							{
+								users.map(user => (
+									<MenuItem key={user.ign}>
+										<ProfilePicture color={user.profilePic}/>
+										<h4>{ user.username }</h4>
+									</MenuItem>
+								))
+							}
+						</Menu>
+					)
+				}
 			</div>
 		</nav>
 	)
